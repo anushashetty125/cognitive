ef7a69113111440d54961ca298abb4e7dfac5e45
anushashetty125 <anushajshetty125@gmail.com>
2022-04-28 20:06:56 +0530

    Session 10

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name="Anusha"
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App firstName={name} age={22} />
+    <App />
   </React.StrictMode>
 );
 
